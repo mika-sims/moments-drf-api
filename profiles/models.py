22c7f32e95273cc11e0d826004e65eba569ea8e3
Mikail Şimşek <mikailsimsek.trb@gmail.com>
2024-01-13 14:06:38 +0100

    Start profiles app and create Profile model

---
+++ profiles/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Profile(models.Model):
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=255, blank=True)
+    content = models.TextField(blank=True)
+    image = models.ImageField(
+        upload_to='images/', default='../default_profile_bxidbs.jpg'
+    )
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self) -> str:
+        return f"{self.owner}'s profile"
