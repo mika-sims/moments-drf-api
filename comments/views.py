6c0f919bf84d45ad5aa4f158963f1a9b0b268a87
Mikail Şimşek <mikailsimsek.trb@gmail.com>
2024-01-15 20:42:30 +0100

    Create CommentList generic view

--- comments/views.py
+++ comments/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from drf_api.permissions import IsOwnerOrReadOnly
+from .models import Comment
+from .serializers import CommentSerializer, CommentDetailSerializer
 
-# Create your views here.
+
+class CommentList(generics.ListCreateAPIView):
+    """
+    API endpoint that represents a list of comments
+    """
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        """
+        Set the owner of the comment to the user that created it
+        """
+        serializer.save(owner=self.request.user)
