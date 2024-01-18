2bf74d62cb55c9016ebb1a5dbd5813d69ec900de
Mikail Şimşek <mikailsimsek.trb@gmail.com>
2024-01-18 19:03:06 +0100

    Added naturaltime formating to created_at and updated_at fields

--- drf_api/serializers.py
+++ drf_api/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.humanize.templatetags.humanize import naturaltime
 from rest_framework import serializers
 from dj_rest_auth.serializers import UserDetailsSerializer
 
@@ -8,6 +9,14 @@ class CurrentUserSerializer(UserDetailsSerializer):
     """
     profile_id = serializers.ReadOnlyField(source='profile.id')
     profile_image = serializers.ReadOnlyField(source='profile.image.url')
+    created_at = serializers.SerializerMethodField()
+    updated_at = serializers.SerializerMethodField()
+    
+    def get_created_at(self, obj):
+        return naturaltime(obj.created_at)
+    
+    def get_updated_at(self, obj):
+        return naturaltime(obj.updated_at)
 
     class Meta(UserDetailsSerializer.Meta):
         fields = UserDetailsSerializer.Meta.fields + (
