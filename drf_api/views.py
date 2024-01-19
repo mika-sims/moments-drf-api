755d77d88be1c35fbee4107dd9bd63c2e70d4c60
Mikail Şimşek <mikailsimsek.trb@gmail.com>
2024-01-19 19:32:51 +0100

    Added deployment changes

--- drf_api/views.py
+++ drf_api/views.py
@@ -1,10 +1,8 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .settings import (
+    JWT_AUTH_COOKIE, JWT_AUTH_REFRESH_COOKIE, JWT_AUTH_SAMESITE,
     JWT_AUTH_SECURE,
-    JWT_AUTH_COOKIE,
-    JWT_AUTH_REFRESH_COOKIE,
-    JWT_AUTH_SAMESITE,
 )
 
 
@@ -15,6 +13,7 @@ def root_route(request):
     })
 
 
+# dj-rest-auth logout view fix
 @api_view(['POST'])
 def logout_route(request):
     response = Response()
